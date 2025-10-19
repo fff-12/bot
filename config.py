60d95fd33b0c04168c9b63d0543b9095c1e144da
Потійчук Максим <161848104+fff-12@users.noreply.github.com>
2025-10-19 17:56:04 +0300

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
-
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-ACCESS_CODE = os.getenv("ACCESS_CODE")
-DATABASE = os.getenv("DATABASE", "database.db")
-SHEET_NAME = os.getenv("SHEET_NAME", "Bot Clients")
